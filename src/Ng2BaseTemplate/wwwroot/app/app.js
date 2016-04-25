bb25b32a1c874552aa9c1b240552b04d455ed13b
Levi Fuller <lfuller941@gmail.com>
2016-04-24 21:16:40 -0600

    Added routing

--- src/Ng2BaseTemplate/wwwroot/app/app.js
+++ src/Ng2BaseTemplate/wwwroot/app/app.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router', './COMPONENTS/home.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,12 +10,18 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, router_1, home_component_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (home_component_1_1) {
+                home_component_1 = home_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -24,8 +30,13 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n        <div>hello there, ng2!</div>\n<input [(ngModel)]=\"myName\" /> {{myName}}\n    "
-                    }), 
+                        template: "\n        <a [routerLink]=\"['Home']\">Home</a>\n        <div>hello there, bro!</div>\n<input [(ngModel)]=\"myName\" /> {{myName}} is great!\n    <router-outlet></router-outlet>\n    ",
+                        directives: [router_1.ROUTER_DIRECTIVES],
+                        providers: [router_1.ROUTER_PROVIDERS]
+                    }),
+                    router_1.RouteConfig([
+                        { name: 'Home', path: '/home', useAsDefault: true, component: home_component_1.HomeComponent }
+                    ]), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
--- src/Ng2BaseTemplate/wwwroot/app/app.ts
+++ src/Ng2BaseTemplate/wwwroot/app/app.ts
@@ -1,12 +1,21 @@
 ﻿import {Component, OnInit} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {HomeComponent} from './COMPONENTS/home.component';
 
 @Component({
     selector: 'my-app',
     template: `
-        <div>hello there, ng2!</div>
-<input [(ngModel)]="myName" /> {{myName}}
-    `
+        <a [routerLink]="['Home']">Home</a>
+        <div>hello there, bro!</div>
+<input [(ngModel)]="myName" /> {{myName}} is great!
+    <router-outlet></router-outlet>
+    `,
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS]
 })
+@RouteConfig([
+        {name: 'Home', path:'/home', useAsDefault: true, component: HomeComponent}
+    ])
 export class AppComponent {
     nyName: string;
 }
